b2f28b218f6d80b076a5b43bfaabd5a742ba1e85
Nazareno <eanselmi@edrans.com>
2022-06-07 20:55:46 -0300

    Add aws install script

--- common/user_data_dc.ps1
+++ common/user_data_dc.ps1
@@ -1,5 +1,15 @@
 <powershell>
-$password2 = $(aws secretsmanager get-secret-value --secret-id password_windows | ConvertTo-SecureString -AsPlainText -Force)
+function prereq {    
+        $command = "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12"
+        Invoke-Expression $command
+        Invoke-WebRequest -Uri "https://awscli.amazonaws.com/AWSCLIV2.msi" -Outfile C:\AWSCLIV2.msi
+        $arguments = "/i `"C:\AWSCLIV2.msi`" /quiet"
+        Start-Process msiexec.exe -ArgumentList $arguments -Wait    
+}
+prereq
+sleep 240
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+$password2 = $(aws secretsmanager get-secret-value --secret-id password_windows --region us-east-1 | ConvertTo-SecureString -AsPlainText -Force)
 new-localuser -name "localadmin2" -password $password2
 add-localgroupmember -group "Remote Desktop Users" -member "localadmin2"
 add-localgroupmember -group "Administrators" -member "localadmin2"
